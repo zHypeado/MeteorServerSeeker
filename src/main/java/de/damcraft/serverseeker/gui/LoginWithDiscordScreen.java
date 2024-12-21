b6cbabd22dca042a51db373d6a7511f43a8969ce
zHypeado <116395242+zHypeado@users.noreply.github.com>
2024-12-20 21:11:21 -0300

    Update LoginWithDiscordScreen.java

--- src/main/java/de/damcraft/serverseeker/gui/LoginWithDiscordScreen.java
+++ src/main/java/de/damcraft/serverseeker/gui/LoginWithDiscordScreen.java
@@ -13,44 +13,6 @@ public class LoginWithDiscordScreen extends WindowScreen {
 
     WindowScreen parent;
 
-    public LoginWithDiscordScreen(WindowScreen parent) {
-        super(GuiThemes.get(), "Login with Discord");
-        this.parent = parent;
-    }
-
-    @Override
-    public void initWidgets() {
-        DiscordAuth.auth((apiKey, error) -> {
-            if (error != null) {
-                this.canClose = true;
-                clear();
-                add(theme.label("Failed to authenticate with Discord. Reason: " + error));
-                return;
-            }
-            if (apiKey == null) {
-                this.canClose = true;
-                clear();
-                add(theme.label("Failed to authenticate with Discord."));
-                return;
-            }
-            close();
-        });
-        add(theme.label("Please authenticate with Discord in your browser."));
-
-        add(theme.label("The browser didn't open? Click below to copy the link and open it manually")).expandX();
-        WButton copy = add(theme.button("Copy")).expandX().widget();
-        copy.action = () -> {
-            String url = DiscordAuth.url;
-            mc.keyboard.setClipboard(url);
-        };
-
-        WButton cancel = add(theme.button("Cancel")).expandX().widget();
-        cancel.action = () -> {
-            DiscordAuth.stopServer();
-            close();
-        };
-    }
-
     @Override
     public void tick() {}
 
